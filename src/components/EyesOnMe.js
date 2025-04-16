ab48a106a99065e7c7804a47d3876dd6c92db01b
Yvonne-del <ys359375@gmail.com>
2025-04-16 08:03:48 +0300

    .

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe(){
+    function eyeFocussed(){
+        console.log("Good!")
+    }
+    function eyeNotFocussed(){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <div>
+            <button onFocus={eyeFocussed} onBlur={eyeNotFocussed} >Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe
